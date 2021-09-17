6151ba7f3b87b95ad2e3d7a1e1e655e8f8f58fe0
ecwpz91 <ecwpz91@users.noreply.github.com>
2021-09-17 11:13:31 -0400

    Initial revision

---
+++ src/main/java/com/redhat/mrp/model/Rover.java
@@ -0,0 +1,113 @@
+package com.redhat.mrp.model;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Rover implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private int id;
+  private String name;
+
+  @JsonProperty("landing_date")
+  private String landingDate;
+
+  @JsonProperty("launch_date")
+  private String launchDate;
+
+  private String status;
+
+  @JsonProperty("max_sol")
+  private String maxSol;
+
+  @JsonProperty("max_date")
+  private String maxDate;
+
+  @JsonProperty("total_photos")
+  private String totalPhotos;
+
+  private Camera[] cameras;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLandingDate() {
+    return landingDate;
+  }
+
+  public void setLandingDate(String landingDate) {
+    this.landingDate = landingDate;
+  }
+
+  public String getLaunchDate() {
+    return launchDate;
+  }
+
+  public void setLaunchDate(String launchDate) {
+    this.launchDate = launchDate;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public String getMaxSol() {
+    return maxSol;
+  }
+
+  public void setMaxSol(String maxSol) {
+    this.maxSol = maxSol;
+  }
+
+  public String getMaxDate() {
+    return maxDate;
+  }
+
+  public void setMaxDate(String maxDate) {
+    this.maxDate = maxDate;
+  }
+
+  public String getTotalPhotos() {
+    return totalPhotos;
+  }
+
+  public void setTotalPhotos(String totalPhotos) {
+    this.totalPhotos = totalPhotos;
+  }
+
+	public Camera[] getCameras() {
+		return cameras;
+	}
+
+	public void setCameras(Camera[] cameras) {
+		this.cameras = cameras;
+	}
+
+  @Override
+	public String toString() {
+		return "{id=" + id + ",name=" + name + ",landingDate=" + landingDate
+				+ ",launchDate=" + launchDate + ",status=" + status + ",maxSol="
+				+ maxSol + ",cameras=" + Arrays.toString(cameras) + "}";
+	}
+
+}
